002f58798ce4315833af7bd53c58e2ed4a58e0ee
aprilianraisyah <aprilianraisyah@gmail.com>
2024-05-15 23:06:58 +0700

    pembuatan fitur alternatif

---
+++ app/Http/Controllers/AlternatifController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Alternatif;
+
+class AlternatifController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+       $dataalternatif = Alternatif::orderBy('id', 'asc')->get();
+        return view('\alternatif\dataalternatif', compact('dataalternatif'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('\alternatif\tambahalternatif');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_alternatif' => 'required'
+        ]);
+
+        Alternatif::create($request->all());
+
+        return redirect()->route('alternatif')
+        ->with('success', 'Data berhasil ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $dataalternatif=Alternatif::find($id);
+        return view('\alternatif\editalternatif', compact('dataalternatif'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'nama_alternatif' => 'required'
+        ]);
+
+        Alternatif::findOrFail($id)->update($request->all());
+
+        return redirect()->route('alternatif')
+        ->with('success', 'Data berhasil diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Alternatif::find($id)->delete();
+        return redirect()->route('alternatif')
+        ->with('success', 'Data berhasil dihapus');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AlternatifController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/*Route::get('/dashboard', function () {
+    return view('dashboard');
+});*/
+
+Route::resource('alternatif', AlternatifController::class);
+
+Route::get('/kriteria', function () {
+    return view('datakriteria');
+});
+
+Route::get('/alternatif', function () {
+    return view('dataalternatif');
+});
+
+Route::get('/penghitungan', function () {
+    return view('penghitungan');
+});
+
+Route::get('/rekomendasi', function () {
+    return view('rekomendasi');
+});
+
+/*Route::get('/login', function () {
+    return view('auth/login');
+});*/
+
+Route::get('/login', [loginController::class, 'index']);
+Route::post('/login/autentikasi', [loginController::class, 'login']);
+
+/*route login
+Route::get('/login', [LoginController::class, 'index'])->name('login');*/
+
+/*route alternatif*/
+Route::get('/alternatif', [AlternatifController::class, 'index'])->name('alternatif');
+
+Route::get('/tambahalternatif', function () {
+    return view('tambahalternatif');
+});
+
+Route::get('/create', [AlternatifController::class, 'create']);
+Route::post('/store', [AlternatifController::class, 'store']);
+
+Route::get('/tambahkriteria', function () {
+    return view('tambahkriteria');
+});
